ac502dce45f6c423ef7fcd630ee722fdbca540b6
tuyalou <63755131+tuyalou@users.noreply.github.com>
2020-04-30 13:50:01 -0500

    Update user_data.sh

--- task3/user_data.sh
+++ task3/user_data.sh
@@ -6,4 +6,3 @@ systemctl enable httpd
 wget https://www.free-css.com/assets/files/free-css-templates/download/page239/black.zip
 unzip black.zip
 mv black/*  /var/www/html/
-mv index.html /var/www/html/
